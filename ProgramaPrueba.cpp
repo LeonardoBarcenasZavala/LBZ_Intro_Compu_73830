b620e2795da99849d06787548b8e8789dd73c53d
LeonardoBarcenasZavala <91902330+LeonardoBarcenasZavala@users.noreply.github.com>
2021-10-06 10:57:33 -0500

    Add files via upload
    Un programa prueba en cpp para trabajar en la terminal.

---
+++ ProgramaPrueba.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib>
+
+int var1=5, var2=8, var3;
+
+int main(){
+
+if(var1!=var2){
+
+printf("Si");
+
+}
+else{
+printf("No");
+}
+
+printf("Escoge una pcion del menu\n");
+printf("Opcion 1\n");
+printf("Opcion 2\n");
+printf("Opcion 3\n");
+scanf(" %i", var1);
+switch(var3)
+
+case 1:
+
+printf("Hola, caso1");
+break;
+
+case 2;
+
+printf("Hola, caso2");
+break;
+
+case 3;
+
+printf("Hola, caso3");
+break;
+
+default:
+
+printf("Eso no existe");
+break;
+
+return 0;
+
+}
